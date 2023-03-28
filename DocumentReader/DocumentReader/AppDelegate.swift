c6a26e771c585f4b0f2918e372b796539de62347
Sharon <sharonbovas@gmail.com>
2023-03-28 07:53:25 +0400

    New XCode Project created

---
+++ DocumentReader/DocumentReader/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  DocumentReader
+//
+//  Created by Sharon Varghese on 28/03/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+
+}
+
